221d8f78176c7ea9176c0fb7c0f6b293885911db
ycberrehouma <ycberrehouma@gmail.com>
2018-11-13 22:57:19 -0500

    Apriori API finished

--- test.py
+++
@@ -1,6 +0,0 @@
-t=[9,127,130,62]
-from itertools import combinations
-print([" ".join(map(str, comb)) for comb in combinations(t,len(t)-1 )])
-t2=[9,127,130]
-x=[item for item in t if item not in t2]
-print(x[0])
